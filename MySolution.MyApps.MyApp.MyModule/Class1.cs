25757e53dd87687e466fe8ed876a8f930d671d70
parveenkhatri1 <parveenkhatri1@hotmail.com>
2015-12-17 08:28:22 +0530

    committign to local repo

---
+++ MySolution.MyApps.MyApp.MyModule/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySolution.MyApps.MyApp.MyModule
+{
+    public class Class1
+    {
+        public static int Sum(int a, int b)
+        {
+            int c;
+
+            c = a + b;
+            return c;
+        }
+    }
+}
